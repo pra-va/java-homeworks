b91f7958704e3650ce8d889b010fc0d2ccfad537
Pranas Varaneckas <varaneckas@gmail.com>
2019-09-23 15:55:49 +0300

    Sukurta HelloWorld programa

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+	/**
+	 * This is my first hello world application done in ECLIPSE IDEA.
+	 */
+
+	public static void main(String[] args) {
+		/**
+		 * This message will print hello world to console!
+		 */
+		System.out.println("Hello, World!!!");
+
+	}
+
+}
