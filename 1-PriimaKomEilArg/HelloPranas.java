b825ac1ffa7c17db2bfddc87f41e2d7f3b7cc4a3
Pranas Varaneckas <varaneckas@gmail.com>
2019-09-23 16:22:16 +0300

    Sukurta Hello [vardas] programa su emuliatorium

---
+++ 1-PriimaKomEilArg/HelloPranas.java
@@ -0,0 +1,10 @@
+/** Programa priims argumenta ir prides pries ji
+*   "hello"
+*/
+
+public class HelloPranas {
+	public static void main(String[] args) {
+		// Atsoaysdubs "Hello [pirmas_argumentas]
+		System.out.println("Hello, " + args[0] + "!");
+	}
+}
