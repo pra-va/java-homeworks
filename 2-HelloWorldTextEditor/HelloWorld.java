e9f469b03f4c0525a54bf0abb52d5aa37ce83146
Pranas Varaneckas <varaneckas@gmail.com>
2019-09-26 14:31:56 +0300

    Sutvarkyta failu struktura

---
+++ 2-HelloWorldTextEditor/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+* The Hello World class implements an application that
+* simply prints "Hello, World!" to standard output.
+*/
+
+class HelloWorld {
+	public static void main(String[] args) {
+		// Prints "Hello, World!" to terminal window.
+		System.out.println("Hello, World!");
+	}
+}
